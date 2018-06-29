f8b6be712334209354d31b13d7d0a13403fae8a5
Exostum <finalserafin@gmail.com>
2018-06-29 12:56:35 +0200

    bug
    Changement des styles de commentaires pour tenter de résoudre le bug du colorateur syntaxique de github

--- index.php
+++ index.php
@@ -26,12 +26,14 @@ unset($chemin_infos);
 // chemin complet en mode de production
 /**
 define('EP_CHEMIN' , dirname(__FILE__). '/../../exoportail' );
-/**/
+/*
+*/
 
 // chemin complet en mode développement
 /**/
 define('EP_CHEMIN' , '/srv/data/web/includes');
-/**/
+/*
+*/
 
 /**
  * Chemin vers le dossier des fichiers PHP d'Exoportail
@@ -48,7 +50,8 @@ else:
 	
 	exit('<!-- [EXOSTUM] [EXOPORTAIL] [test] [file_exists] [' . EP_CHEMIN_PHP . 'index' . EP_FICHIER_EXT .']. [KO]  -->');
 endif;
-/**/
+/*
+*/
 
 set_include_path(EP_CHEMIN_PHP);
 require_once('exoportail' . EP_FICHIER_EXT);
